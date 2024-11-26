78c02fdfff6e9214c71566923a59b19d4474ea2b
javierahumada4 <javierahumada1195@gmail.com>
2024-11-26 19:46:06 +0100

    Calibration pictures

--- picture.py
+++ picture.py
@@ -14,17 +14,22 @@ def format_camera():
     return picam
 
 def make_picture(camera):
-    picture = camera.capture_array()
-    cv2.imshow("picam", picture)
-    return picture
+    while True:
+        frame = camera.capture_array()
+        cv2.imshow("picam", frame)
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            #cv2.imwrite(path_, frame)
+            break
+    return frame
 
 def write_image(img, path):
     cv2.imwrite(path, img)
 
 if __name__ == "__main__":
     camera = format_camera()
-    for i in range(10):
-        path = join(dirname(getcwd()), "data")
-        videopath = join(path, f"corners_{i}.png")
-        write_image(make_picture(camera), path)
+    path = dirname(getcwd())
+    for i in range(30):
+        picture = make_picture(camera)
+        path_ = join(path, f"JustDance_ComputerVision-1/chess{i}.png")
+        write_image(picture, path_)
     print("Making photos endeded")
