dbe3ecfd5d9ca0ba407206f2dac486e8ff8031bf
javierahumada4 <202213736@alu.comillas.edu>
2025-01-02 15:24:51 +0100

    trackers first edition

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ def stream_video():
     # Define orange color range
     light_orange = (10, 25, 60)
     dark_orange = (35, 180, 215)
+    
+    trackers = []
 
     picam = Picamera2()
     picam.preview_configuration.main.size=(1280, 720)
@@ -21,9 +23,33 @@ def stream_video():
         # Compute a list that contains a mask (which should segment orange colors) for every image.
         img_mask = cv2.inRange(hsv_img, light_orange, dark_orange)
         # Compute a list that contains the result of multiplying the original image with its orange colors mask.
-        img_segmented = cv2.bitwise_and(frame, frame, mask=img_mask)
+        # img_segmented = cv2.bitwise_and(frame, frame, mask=img_mask) ESTA LINEA NO ES NECESARIA, PODEMOS USAR LA MASCARA DIRECTAMENTE PARA ENCONTRAR LOS CONTORNOS
+        
+        # Encuentra contornos en la máscara
+        contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Dibuja rectángulos alrededor de los objetos segmentados
+        boxes = []
+        for contour in contours:
+            if cv2.contourArea(contour) > 500:  # Filtra por área mínima
+                x, y, w, h = cv2.boundingRect(contour)
+                boxes.append((x, y, x+w, y+h))
 
-        cv2.imshow("picam", img_segmented)
+        # Si no hay trackers inicializados, inicialízalos
+        if not trackers:
+            for box in boxes:
+                tracker = cv2.TrackerKCF_create()  # Puedes usar otro tipo de tracker aquí
+                trackers.append(tracker)
+                tracker.init(frame, tuple(box))  # Inicializa el tracker con el cuadro delimitador
+
+        # Actualiza la posición de los trackers
+        for tracker in trackers:
+            success, box = tracker.update(frame)
+            if success:
+                x, y, w, h = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+        cv2.imshow("picam", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.destroyAllWindows()
