5c9123e4afeef1ffc469d8f83c2592034da0ebb2
javierahumada4 <javierahumada1195@gmail.com>
2025-01-02 15:55:36 +0100

    trackers fourth edition

--- game.py
+++ game.py
@@ -22,14 +22,14 @@ def stream_video():
         # Compute a list that contains a mask (which should segment orange colors) for every image.
         img_mask = cv2.inRange(hsv_img, light_orange, dark_orange)
         # Compute a list that contains the result of multiplying the original image with its orange colors mask.
-        # img_segmented = cv2.bitwise_and(frame, frame, mask=img_mask) ESTA LINEA NO ES NECESARIA, PODEMOS USAR LA MASCARA DIRECTAMENTE PARA ENCONTRAR LOS CONTORNOS
+        img_segmented = cv2.bitwise_and(frame, frame, mask=img_mask) # ESTA LINEA NO ES NECESARIA, PODEMOS USAR LA MASCARA DIRECTAMENTE PARA ENCONTRAR LOS CONTORNOS
         
         # Encuentra contornos en la máscara
         contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # Dibuja rectángulos alrededor de los objetos segmentados
         detected_boxes = []
         for contour in contours:
-            if cv2.contourArea(contour) > 500:  # Filtra por área mínima
+            if cv2.contourArea(contour) > 5000:  # Filtra por área mínima
                 x, y, w, h = cv2.boundingRect(contour)
                 detected_boxes.append((x, y, x + w, y + h))
         for box in detected_boxes:
